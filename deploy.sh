ab411565a05dea16140676b0e97ac81df54509ef
David Susskind <david.s@noogata.com>
2023-06-08 20:41:34 +0300

    minor improvement in deploy flow

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ sed -i.bak 's/assets/spotLabel\/assets/' ./dist/index.html
 mkdir ./dist/accept
 cp ./dist/index.html ./dist/accept
 mv ./dist/* ./
-git commit . -m 'deploy'
+git add .
+git commit -m 'deploy'
 git push -f
