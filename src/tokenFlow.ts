2a7d8afa38dbddad3c32c7b0b94ae18201e12a7c
David Susskind <david.s@noogata.com>
2023-06-08 21:24:11 +0300

    prod vs dev

--- src/tokenFlow.ts
+++ src/tokenFlow.ts
@@ -50,7 +50,7 @@ const clientId = import.meta.env.PROD
   ? '08bb5e52ec0d4e6aaf8c45655cc8bdc4'
   : 'a8072301988e40ee9439837c265631fd'
 const home = window.location.origin
-const redirectUri = `${home}/accept`
+const redirectUri = `${home}${import.meta.env.PROD ? '/spotLabel' : ''}/accept`
 
 let codeVerifier = generateRandomString(128)
 export function getAuthorizationCode() {
